7386936e1f4b59b093d9e6d231930ea4fa9736fd
Administrator <hastycodeandreh@gmail.com>
2024-05-30 09:42:09 +0300

    Added !

--- src/main/java/Sandbox.java
+++ src/main/java/Sandbox.java
@@ -3,7 +3,7 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
-        System.out.println("Welcome to Programming");
+        System.out.println("Welcome to Programming!");
         
     }
 }
